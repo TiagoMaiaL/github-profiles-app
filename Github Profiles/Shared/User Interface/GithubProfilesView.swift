6be8af642d6fe5d2248484b5359e3dd0f4f72886
Tiago Maia <tiago.maia.lopes@gmail.com>
2021-08-08 18:55:09 -0300

    Add the initial views

--- Github Profiles/Shared/User Interface/GithubProfilesView.swift
+++ Github Profiles/Shared/User Interface/GithubProfilesView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct GithubProfilesView: View {
+    
+    // MARK: Properties
+    
+    @State
+    private var searchText: String = ""
+    
+    // MARK: Body
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            searchBar
+            Spacer()
+            profileView
+            Spacer()
+        }
+    }
+    
+    private var searchBar: some View {
+        HStack {
+            TextField("Search for a Github profile", text: $searchText)
+            Image(systemName: "magnifyingglass")
+        }
+        .padding()
+        .background(.thickMaterial)
+    }
+    
+    private var profileView: some View {
+        VStack {
+            Image("octocat_image")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Text("The Octocat")
+                .font(.title)
+            
+            Text("Company")
+            Text("Blog")
+            Text("Location")
+            
+        }
+        .padding()
+        .background(.thickMaterial)
     }
 }
 
